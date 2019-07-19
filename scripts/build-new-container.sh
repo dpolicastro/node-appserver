e8fad59fdf96921cb8518a5a589a6d8b3ccaea41
Denis Policastro <denis.policastro@gmail.com>
2019-07-18 21:28:57 -0300

    Add env var to startup script

--- scripts/build-new-container.sh
+++ scripts/build-new-container.sh
@@ -1,6 +1,8 @@
+export APP_FOLDER=/app
 echo "Starting buiding container for env: $DEPLOYMENT_GROUP_NAME"
 if [ "$DEPLOYMENT_GROUP_NAME" == "Staging" ]; then
-  docker-compose -f ../docker-compose.yaml up -d --build
+  ls
+  docker-compose -f "$APP_FOLDER/docker-compose.yaml" up -d --build
 elif [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]; then
   echo "Development"
 fi
