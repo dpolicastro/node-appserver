93c3ff361009290e638a4978de5834ac01f97717
Denis Policastro <denis.policastro@gmail.com>
2019-07-18 21:08:10 -0300

    retry shell script

--- scripts/build-new-container.sh
+++ scripts/build-new-container.sh
@@ -1,9 +1,6 @@
+echo "Starting buiding container for env: $DEPLOYMENT_GROUP_NAME"
 if [ "$DEPLOYMENT_GROUP_NAME" == "Staging" ]; then
   docker-compose -f docker-compose.yaml up -d --build
 elif [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]; then
-  # Copy to /var/www/development
-elif [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]; then
-  # Copy to /var/www/html
-else
-  # Fail the deployment
+  echo "Development"
 fi
